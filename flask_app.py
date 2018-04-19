67fc520f0a125523b26ee9e07175ffb42386cea1
wendyytam <wendytam@24dcaea7d349>
2018-04-19 15:49:13 +0000

    First commit

---
+++ flask_app.py
@@ -0,0 +1,23 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Wendy!'
+
+@app.route('/about_me')
+@app.route('/aboutme')
+def about_me():
+    return app.send_static_file('about_me.html')
+
+@app.route('/class_schedule')
+def class_schedule():
+    return app.send_static_file('class_schedule.html')
+
+@app.route('/register')
+def register():
+    return app.send_static_file('register.html')
